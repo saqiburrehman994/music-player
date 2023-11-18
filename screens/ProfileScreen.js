1b3500c5c507ed3bbb48784514d2a4588f73cd39
Saqib Ur Rehman <saqibrehman062@gmail.com>
2023-11-18 11:41:21 +0500

    First Screen for Login is done , so I am commiting it to git.

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet,Text , View } from "react-native";
+import React from "react";
+const ProfileScreen = ()=>{
+    return(
+        <View>
+            <Text>
+                Profile Screen
+            </Text>
+        </View>
+    )
+}
+export default ProfileScreen
+const styles = StyleSheet.create({})
